172aeb763a05ff3b7924458eb351a5460e48688b
qualitycodebase <qualitycodebase@gmail.com>
2020-05-15 01:50:36 -0400

    Lists volumes and snapshots. Native implemetation of snapshots command

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ In my case "shotty" or anyname you provide with config file, uses the configurat
 `aws configure --profile shotty`
 
 ## How to run it
-`pipenv run python shotty/shotty.py <command> <--project=PROJECT>`
+`pipenv run python shotty/shotty.py <command> <subcommand> <--project=PROJECT>`
 
-*command* is list, start, or stop
+
+*command* is instances, volumes, or snapshots
+*subcommand* - depends on commands=
 *project* is optional
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,8 +16,77 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manages snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+     help="Only snapshots for project (tag Project:<name>)")
+
+def list_snapshots(project):
+    "List snapshots instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+     help="Only volumes for project (tag Project:<name>)")
+
+def list_volumes(project):
+    "List volumes instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
+
+@instances.command('snapshot',
+      help="Create snapshots for all volumes")
+@click.option('--project', default=None,
+     help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshots(Description="Created by snapshotanalyzer 2020")
+    return
+
 @instances.command('list')
 @click.option('--project', default=None,
      help="Only instances for project (tag Project:<name>)")
@@ -62,4 +131,4 @@ def start_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
